9644f65c4f74f92ee8bc8243d6f87801f3aed9f4
saff-it <saff_it@ymail.com>
2022-09-08 16:39:00 +0200

    route list working +bootstrap imported

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    //
     public function index(){
-        $movies = Movie::all();
-        dd($movies);
-        return view ('home', compact('movies'));
+        return view ('home');
     }
+    
 }
---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    //
+    public function index(){
+        
+        $movies = Movie::all();
+        
+        return view ('movies.index', compact('movies'));
+    }
+}
