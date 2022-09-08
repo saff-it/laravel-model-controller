d0505ca7c8499bdc4e53ace4935758cd676ff049
saff-it <saff_it@ymail.com>
2022-09-08 16:11:41 +0200

    movie model created and connected to controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view ('home');
+        $movies = Movie::all();
+        dd($movies);
+        return view ('home', compact('movies'));
     }
 }
