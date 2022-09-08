be38729dffb022e327e860dac2d07f6c8a17a857
saff-it <saff_it@ymail.com>
2022-09-08 17:40:50 +0200

    cards populated dynamically

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.main')
 
 @section('main-content')
-
     <div>
-        <ul>
-            @foreach ($movies as $movie)
-            <li>
-                {{$movie -> id}} - {{$movie -> title}}
-            </li>
-                
-            @endforeach
-        </ul>
+        @foreach ($movies as $movie)
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{$movie -> title}}</h5>
+                    <p class="card-text">Il titolo originale di questo film era {{$movie -> original_title}}</p>
+                </div>
+            </div>
+        @endforeach
     </div>
-
 @endsection
